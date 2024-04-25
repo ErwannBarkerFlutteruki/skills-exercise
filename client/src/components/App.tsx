ae13315df183a4f36854d955eecd94b4be519620
erwann barker <erwann.barker@flutteruki.com>
2024-04-25 11:17:41 +0100

    wip by erwann

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,20 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component, ReactElement } from 'react'
 
 import { wsEndpoint } from '../utils/config'
+import Market from './Market'
 
-export class App extends Component<{}, { data: any }> {
+export  class App extends Component<{}, { data: any }> {
   private w: WebSocket
   constructor(props: any) {
 
     super(props)
     this.w = new WebSocket(wsEndpoint)
     this.state = {
-      data: {data: null}
-    }
-  }
-	
+      	data: {
+			liveEventsData: [],
+			market: {}
+		}
+	}
+}
+
 	/* Toggle Odds
-	
+
 	export default App => {
 		const [toggled, isToggled] = useState();
 		...
@@ -25,39 +29,80 @@ export class App extends Component<{}, { data: any }> {
 	})
 
 	return (
-		<button onClick = () /> 
+		<button onClick = () />
 	)
 
 	*/
 
+
   componentDidMount() {
+	//example of types
+	type LiveEventDataResponseType = {
+		eventId: number;
+	}
+
     // a message has been recieved
     this.w.onmessage = (e: MessageEvent) => {
-			this.setState({
-				data:JSON.parse(e.data)				
-			})
+		const response = JSON.parse(e.data);
+		// console.log(response.type);
+		switch (response.type) {
+			case "LIVE_EVENTS_DATA" :
+				const eventData: LiveEventDataResponseType = response.data;
+				this.setState({
+					data: {
+						...this.state.data,
+						liveEventsData: eventData,
+						market: null
+					}
+				});
+			break;
+			case "MARKET_DATA" :
+				const marketData: LiveEventDataResponseType = response.data;
+				this.setState({
+					data: {
+						...this.state.data,
+						market: marketData
+					}
+				}, this.getOutcomes());
+			break;
+
+		}
+
     }
     // send a request to get events
     this.w.onopen = () =>
-      this.w.send(JSON.stringify({type: "getLiveEvents", primaryMarkets: true }));
-  } 	
+      	this.w.send(JSON.stringify({type: "getLiveEvents", primaryMarkets: true }));
+  	}
+
+	getMarket = (marketId: number) => {
+		this.w.send(JSON.stringify({ type: "getMarket", id: marketId }));
+   }
   componentWillUnmount() {
     this.w.close()
-  } 
+  }
 
   render() {
-		const { data } = this.state
-		console.log(data.data);
-		if (data && data.type && data.type === 'LIVE_EVENTS_DATA') {	
+	const { data } = this.state
+
+	  if (data && data.liveEventsData.length) {
 			// console.log(data.data.map((item: any) => item.name));
 			return (
 				<div>
-					{JSON.stringify(data.data.map((item: any) => item.name ))}
-						<div> + </div>
-				</div>
-				/* <div>{JSON.stringify(data.data)} */
+					{data.liveEventsData.map((item: any) => {
+						let market: any = null;
+						if (this.state.data.market && item.eventId === this.state.data.market.eventId) {
+							market = <Market market={data.market}/>
+						}
+						return (
+						<>
+							<div key={item.eventId} onClick={() => this.getMarket(item.markets[0])}>{item.name}</div>
+							{market}
+						</>
+						)
+	  })}
 
-			) 
+				</div>
+			)
 		} else
 		return <div>loading</div>
 	}}
---
+++ client/src/components/Market.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+const Market: React.FC<any> = (props) => {
+	console.log(props)
+	return (
+		<>hello market</>
+	)
+}
+
+
+export default Market;
